2d5244d05e7f201f91354591a756e6a49d26f967
Sebastián <sreguera@gmail.com>
2011-11-12 23:21:05 +0100

    Added copyright header.

--- transputer/src/jsrc/sim/transputer/Main.java
+++ transputer/src/jsrc/sim/transputer/Main.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright 2011 Jose Sebastian Reguera Candal
+ *
+ *  This file is part of VPK.
+ *
+ *  VPK is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  VPK is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with VPK.  If not, see <http://www.gnu.org/licenses/>. 
+ */
 package jsrc.sim.transputer;
 
 public class Main {
